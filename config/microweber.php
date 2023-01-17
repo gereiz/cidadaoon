182a3ad6f17ba3dde59b253c7dc8641bf6fc03bf
Georgie Reis <georgie.reis@farmaciaindiana.far.br>
2023-01-17 09:26:16 -0300

    Commit Inicial

---
+++ config/microweber.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'version' => '1.3.1',
+  'install_default_template' => 'new-world',
+  'install_default_template_content' => 1,
+  'compile_assets' => 1,
+  'disable_model_cache' => 0,
+  'admin_url' => 'admpainel',
+  'site_lang' => 'en_US',
+  'has_admin' => 1,
+  'is_installed' => 1,
+  'updated_at' => '2023-01-14 13:07:57',
+);
